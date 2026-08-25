de1a2d57cd8d7dd998c7af28b046d3a552b2aef9
BrunoSevero-188 <brunosevero188@gmail.com>
2026-08-25 19:51:16 -0400

    Atualização: 25-08

--- app/telas/TelasInternas/slideBar/slideBarSecundarios/EditarPerfil.page.tsx
+++ app/telas/TelasInternas/slideBar/slideBarSecundarios/EditarPerfil.page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { Input } from "@/components/input";
+import { InputandLabel } from "@/components/inputandLabel";
 import { Button } from "@/components/button";
 import { useRouter } from "next/navigation";
 import { dadosUsuario } from "@/components/dadosUsuario";
 import styleSlideBar from "@/ConjuntosCss/TelasCss/SlideBar.module.css";
+import styleInput from "@/ConjuntosCss/ComponentesCss/Input.module.css";
 
 export default function EditarPerfil() {
   const router = useRouter();
@@ -31,24 +32,55 @@ export default function EditarPerfil() {
       <div className={styleSlideBar.paginaCartaoFormulario}>
         <h1 className={styleSlideBar.paginaTituloEscuro}>Editar Perfil</h1>
 
-        <div className={styleSlideBar.formularioGrid}>
-          <Input label="Nome Completo" value={usuario.nomeCompleto}
-            onChange={(e) => atualizar("nomeCompleto", e.target.value)} />
-
-          <Input label="Nome do Perfil" value={usuario.nomePerfil}
-            onChange={(e) => atualizar("nomePerfil", e.target.value)} />
-
-          <Input label="CPF" value={usuario.cpf}
-            onChange={(e) => atualizar("cpf", e.target.value)} />
-
-          <Input label="E-mail" value={usuario.email}
-            onChange={(e) => atualizar("email", e.target.value)} />
-
-          <Input label="Cargo" value={usuario.cargo}
-            onChange={(e) => atualizar("cargo", e.target.value)} />
-
-          <Input label="Estabelecimento" value={usuario.estabelecimento}
-            onChange={(e) => atualizar("estabelecimento", e.target.value)} />
+        {/* Mesma configuração de input do Criar Usuario/Acessar Perfil:
+            className/containerClassName explícitos, pra usar as classes de
+            foco e label flutuante corretas (sem isso o label não encolhe
+            e fica em cima do valor). */}
+        <div className={styleInput.containerOrdenaçãoInputs}>
+          <div className={styleInput.containerInputs}>
+            <InputandLabel
+              label="Nome Completo"
+              value={usuario.nomeCompleto}
+              onChange={(e) => atualizar("nomeCompleto", e.target.value)}
+              className={styleInput.containerElementoInput}
+              containerClassName={styleInput.containerElementoContainer}
+            />
+            <InputandLabel
+              label="Nome do Perfil"
+              value={usuario.nomePerfil}
+              onChange={(e) => atualizar("nomePerfil", e.target.value)}
+              className={styleInput.containerElementoInput}
+              containerClassName={styleInput.containerElementoContainer}
+            />
+            <InputandLabel
+              label="CPF"
+              value={usuario.cpf}
+              onChange={(e) => atualizar("cpf", e.target.value)}
+              className={styleInput.containerElementoInput}
+              containerClassName={styleInput.containerElementoContainer}
+            />
+            <InputandLabel
+              label="E-mail"
+              value={usuario.email}
+              onChange={(e) => atualizar("email", e.target.value)}
+              className={styleInput.containerElementoInput}
+              containerClassName={styleInput.containerElementoContainer}
+            />
+            <InputandLabel
+              label="Cargo"
+              value={usuario.cargo}
+              onChange={(e) => atualizar("cargo", e.target.value)}
+              className={styleInput.containerElementoInput}
+              containerClassName={styleInput.containerElementoContainer}
+            />
+            <InputandLabel
+              label="Estabelecimento"
+              value={usuario.estabelecimento}
+              onChange={(e) => atualizar("estabelecimento", e.target.value)}
+              className={styleInput.containerElementoInput}
+              containerClassName={styleInput.containerElementoContainer}
+            />
+          </div>
         </div>
 
         <Button onClick={salvar}>Salvar Alteracoes</Button>
